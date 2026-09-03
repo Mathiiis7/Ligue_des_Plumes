24446f3934e4122f4693776b6b7677c38847da2e
Mathis Burgevin <mathis.burgevin@gmail.com>
2026-09-03 14:42:42 +0200

    Birdydex missing cards : fond gris uniforme uni (var --line-2) au lieu du degrade, pour mieux distinguer visuellement les cases cochees. Bump SW v13->v14.

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 //   puis rafraichit en background. Prochain reload = nouvelle version.
 // - Requetes cross-origin (Firestore, iNaturalist, xeno-canto, Wikipedia, etc.) : reseau seul.
 // - Bump CACHE_VERSION quand on veut invalider volontairement.
-const CACHE_VERSION = 'v13-2026-09-03-pkdx-missing-clean';
+const CACHE_VERSION = 'v14-2026-09-03-pkdx-flat-grey';
 const CACHE_NAME = 'lmb-' + CACHE_VERSION;
 
 self.addEventListener('install', (e) => {
