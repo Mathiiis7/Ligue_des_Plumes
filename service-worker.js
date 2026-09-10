c1de4016748b8a5ddc56a12617de5620ce495248
Mathis Burgevin <mathis.burgevin@gmail.com>
2026-09-11 00:53:02 +0200

    UI : 'Retirer ma liste de la ligue' passe de linkbtn (texte souligne) a un vrai bouton ghost destructif : icone 🗑️, bordure fine, texte gris, hover en rouge doux. Aligne a droite dans son container. Discret mais elegant. Cache v70.

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 //   puis rafraichit en background. Prochain reload = nouvelle version.
 // - Requetes cross-origin (Firestore, iNaturalist, xeno-canto, Wikipedia, etc.) : reseau seul.
 // - Bump CACHE_VERSION quand on veut invalider volontairement.
-const CACHE_VERSION = 'v69-2026-09-11-tu-prenom';
+const CACHE_VERSION = 'v70-2026-09-11-remove-list-button-styled';
 const CACHE_NAME = 'lmb-' + CACHE_VERSION;
 
 self.addEventListener('install', (e) => {
