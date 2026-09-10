4a04bd270ad0e0fc6f3d904046daa1e0ffbce28c
Mathis Burgevin <mathis.burgevin@gmail.com>
2026-09-11 00:11:33 +0200

    Fix avatars pas centres. (1) .user-avatar : line-height:1 + text-align:center pour eviter le decalage baseline sur emoji/initiale. (2) .prof-avatar-preview : box-sizing:border-box + child en width:100%/height:100% (au lieu de 72px sur les 2) pour que le child remplisse exactement l'inner content-box. (3) pcard-avatar-wrap line-height:1. Cache v59.

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 //   puis rafraichit en background. Prochain reload = nouvelle version.
 // - Requetes cross-origin (Firestore, iNaturalist, xeno-canto, Wikipedia, etc.) : reseau seul.
 // - Bump CACHE_VERSION quand on veut invalider volontairement.
-const CACHE_VERSION = 'v58-2026-09-11-chat-avatar-spacing';
+const CACHE_VERSION = 'v59-2026-09-11-avatar-center-fix';
 const CACHE_NAME = 'lmb-' + CACHE_VERSION;
 
 self.addEventListener('install', (e) => {
